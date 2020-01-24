741c80b7eec7618ac7b6ea2325a055ee768e115f
Nikola Lukic <nik.lukic.93@gmail.com>
2020-01-24 09:39:14 +0100

    inicijalni commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-met-hotels';
+  articles: Article[]; //osobina komponente
+  constructor() {
+  this.articles = [
+  new Article('IT255 - Veb sistemi 1', 'http://www.metropolitan.ac.rs', 3),
+  new Article('Fullstack', 'http://fullstack.io', 2),
+  new Article('Angular Homepage', 'http://angular.io', 1),
+  ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+    }
 }
